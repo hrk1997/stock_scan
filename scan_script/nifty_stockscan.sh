f75f3d6d2c32cba865ad0f7207fec398cb52ea84
Hrithik Krishna <hrk999666333@gmail.com>
2021-08-04 22:18:02 +0530

    setting up this repository

---
+++ scan_script/nifty_stockscan.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+date >> /home/hrk/Desktop/stock_scan/scan_log/niftystockscan.txt
+
+python adaniport_scan.py   >> /home/hrk/Desktop/stock_scan/scan_data/niftystockscan/adaniport.csv
+python airtel_scan.py      >> /home/hrk/Desktop/stock_scan/scan_data/niftystockscan/airtel.csv
+python asianpaint_scan.py  >> /home/hrk/Desktop/stock_scan/scan_data/niftystockscan/asianpaint.csv
+python axisbank_scan.py    >> /home/hrk/Desktop/stock_scan/scan_data/niftystockscan/axisbank.csv
+python bajajauto_scan.py   >> /home/hrk/Desktop/stock_scan/scan_data/niftystockscan/bajajauto.csv
+python bajajfinance_scan.py   >> /home/hrk/Desktop/stock_scan/scan_data/niftystockscan/bajajfinance.csv
+python bajajfinser_scan.py    >> /home/hrk/Desktop/stock_scan/scan_data/niftystockscan/bajajfinser.csv
+python bpcl_scan.py       >> /home/hrk/Desktop/stock_scan/scan_data/niftystockscan/bpcl.csv
+python britania_scan.py  >> /home/hrk/Desktop/stock_scan/scan_data/niftystockscan/britania.csv
+python cipla_scan.py      >> /home/hrk/Desktop/stock_scan/scan_data/niftystockscan/cipla.csv
+python coalindia_scan.py  >> /home/hrk/Desktop/stock_scan/scan_data/niftystockscan/coalindia.csv
+python divislab_scan.py   >> /home/hrk/Desktop/stock_scan/scan_data/niftystockscan/divislab.csv
+python drreddy_scan.py    >> /home/hrk/Desktop/stock_scan/scan_data/niftystockscan/drreddy.csv
+python eichermotors_scan.py   >> /home/hrk/Desktop/stock_scan/scan_data/niftystockscan/eichermotors.csv
+python grasim_scan.py     >> /home/hrk/Desktop/stock_scan/scan_data/niftystockscan/grasim.csv
+python hcltech_scan.py    >> /home/hrk/Desktop/stock_scan/scan_data/niftystockscan/hcltech.csv
+python hdfcbank_scan.py   >> /home/hrk/Desktop/stock_scan/scan_data/niftystockscan/hdfcbank.csv
+python hdfclife_scan.py   >> /home/hrk/Desktop/stock_scan/scan_data/niftystockscan/hdfclife.csv
+python hdfc_scan.py       >> /home/hrk/Desktop/stock_scan/scan_data/niftystockscan/hdfc.csv
+python heromotor_scan.py  >> /home/hrk/Desktop/stock_scan/scan_data/niftystockscan/heromotor.csv
+python hindalco_scan.py   >> /home/hrk/Desktop/stock_scan/scan_data/niftystockscan/hindalco.csv
+python hindunilvar_scan.py   >> /home/hrk/Desktop/stock_scan/scan_data/niftystockscan/hindunilvar.csv
+python icicibank_scan.py     >> /home/hrk/Desktop/stock_scan/scan_data/niftystockscan/icicibank.csv
+python indusindbank_scan.py  >> /home/hrk/Desktop/stock_scan/scan_data/niftystockscan/indusindbank.csv
+python infosys_scan.py   >> /home/hrk/Desktop/stock_scan/scan_data/niftystockscan/infosys.csv
+python ioc_scan.py       >> /home/hrk/Desktop/stock_scan/scan_data/niftystockscan/ioc.csv
+python itc_scan.py       >> /home/hrk/Desktop/stock_scan/scan_data/niftystockscan/itc.csv
+python jswsteel_scan.py  >> /home/hrk/Desktop/stock_scan/scan_data/niftystockscan/jswsteel.csv
+python kodakmahindra_scan.py   >> /home/hrk/Desktop/stock_scan/scan_data/niftystockscan/kodakmahindra.csv
+python larsen_scan.py   >> /home/hrk/Desktop/stock_scan/scan_data/niftystockscan/larsen.csv
+python maruti_scan.py   >> /home/hrk/Desktop/stock_scan/scan_data/niftystockscan/maruti.csv
+python mahindra_scan.py   >> /home/hrk/Desktop/stock_scan/scan_data/niftystockscan/mahindra.csv
+python nestle_scan.py     >> /home/hrk/Desktop/stock_scan/scan_data/niftystockscan/nestle.csv
+python ntpc_scan.py       >> /home/hrk/Desktop/stock_scan/scan_data/niftystockscan/ntpc.csv
+python ongc_scan.py       >> /home/hrk/Desktop/stock_scan/scan_data/niftystockscan/ongc.csv
+python powergrid_scan.py >> /home/hrk/Desktop/stock_scan/scan_data/niftystockscan/powergrid.csv
+python reliance_scan.py  >> /home/hrk/Desktop/stock_scan/scan_data/niftystockscan/reliance.csv
+python sbilife_scan.py   >> /home/hrk/Desktop/stock_scan/scan_data/niftystockscan/sbilife.csv
+python sbi_scan.py       >> /home/hrk/Desktop/stock_scan/scan_data/niftystockscan/sbi.csv
+python shreecement_scan.py >> /home/hrk/Desktop/stock_scan/scan_data/niftystockscan/shreecement.csv
+python sunpharma_scan.py   >> /home/hrk/Desktop/stock_scan/scan_data/niftystockscan/sunpharma.csv
+python tataconsum_scan.py  >> /home/hrk/Desktop/stock_scan/scan_data/niftystockscan/tataconsum.csv
+python tatamotors_scan.py  >> /home/hrk/Desktop/stock_scan/scan_data/niftystockscan/tatamotors.csv
+python tatasteel_scan.py   >> /home/hrk/Desktop/stock_scan/scan_data/niftystockscan/tatasteel.csv
+python tcs_scan.py         >> /home/hrk/Desktop/stock_scan/scan_data/niftystockscan/tcs.csv
+python techmahinfra_scan.py   >> /home/hrk/Desktop/stock_scan/scan_data/niftystockscan/techmahinfra.csv
+python titan_scan.py     >> /home/hrk/Desktop/stock_scan/scan_data/niftystockscan/titan.csv
+python ultatech_scan.py  >> /home/hrk/Desktop/stock_scan/scan_data/niftystockscan/ultatech.csv
+python upl_scan.py       >> /home/hrk/Desktop/stock_scan/scan_data/niftystockscan/upl.csv
+python wipro_scan.py     >> /home/hrk/Desktop/stock_scan/scan_data/niftystockscan/wipro.csv
+
+date >> /home/hrk/Desktop/stock_scan/scan_log/niftystockscan.txt
